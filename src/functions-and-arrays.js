d06e6ab73d05f39bf9effd73062f4438ff46eb21
Lorena Sarasua <lorena.sarasua@gmail.com>
2022-06-10 01:02:03 -0400

    remove comments. Passed all tests

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -7,12 +7,6 @@ function maxOfTwoNumbers(num1, num2) {
   }
 }
 
-// console.log(maxOfTwoNumbers(2, 1)); // 2
-// console.log(maxOfTwoNumbers(5, -7)); // 5
-// console.log(maxOfTwoNumbers(1, 3)); // 3
-// console.log(maxOfTwoNumbers(-5, 3)); // 3
-// console.log(maxOfTwoNumbers(4, 4)); // 4
-
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
 
@@ -29,14 +23,6 @@ function findLongestWord(arrWords) {
   return longest;
 }
 
-// console.log(findLongestWord(words)); // crocodile
-// console.log(findLongestWord([])); // null
-// console.log(findLongestWord(['foo'])); // foo
-// console.log(findLongestWord(['foo', 'bar'])); // foo
-// console.log(findLongestWord(['bar', 'foo'])); // bar
-// console.log(findLongestWord(['a', 'zab', '12abc', '$$abcd', 'abcde', 'ironhack'])); // ironhack
-
-
 // Iteration #3: Calculate the sum
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
@@ -50,14 +36,6 @@ function sumNumbers(arrNum) {
   });
   return sum;   
 }
-
-// console.log(sumNumbers(numbers)); // 87
-// console.log(sumNumbers([])); // 0
-// console.log(typeof sumNumbers); //function
-// console.log(sumNumbers([4])); // 4
-// console.log(sumNumbers([0, 0, 0, 0, 0])); // 0
-// console.log(sumNumbers([10, 5, 4, 32, 8])); // 59
-
 
 // Iteration #3.1 Bonus:
 const mixedArr = [6, 12, 'miami', 1, true, 'barca', '200', 'lisboa', 8, 10]; 
@@ -80,15 +58,6 @@ function sum(mixArr) {
   return sum;
 }
 
-// console.log(sum(mixedArr)); // 57
-// console.log(sum([4])); // 4
-// console.log(sum([10, 5, 4, 32, 8])); //59
-// console.log(sum(['ana', 'marco', 'nicolas', 'tania', 'ptwd'])); // 24
-// console.log(sum([6, 12, 'miami', 1, 'barca', '200', 'lisboa', 8, 10])); // 56
-// console.log(sum([6, 12, 'miami', 1, 'barca', '200', 'lisboa', 8, false])); // 46
-// console.log(sum([6, 12, 'miami', 1, 'barca', '200', 'lisboa', 8, true])); // 47
-console.log(sum([6, 12, 'miami', 1, 'barca', '200', 'lisboa', 8, [], {}])); // Error("Unsupported data type sir or ma'am")
-
 // Iteration #4: Calculate the average
 // Level 1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
@@ -101,9 +70,6 @@ function averageNumbers(numArr) {
   return Math.round(sum/numArr.length);
 }
 
-// console.log(averageNumbers(numbersAvg)); // 6
-// console.log(averageNumbers([9, -3, -4, 6])); // 2
-// console.log(averageNumbers([9, 10, 82, 92, 32, 102, 58])); // 55
 
 // Level 2: Array of strings
 const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
@@ -116,10 +82,6 @@ function averageWordLength(wordArr) {
   return Math.round(totalLength/wordArr.length);
 }
 
-// console.log(averageWordLength(wordsArr)); // 5
-// console.log(averageWordLength(['ironhack'])); // 8
-// console.log(averageWordLength(['Ironhack', 'Madrid', 'Barcelona', 'Paris', 'Miami', 'Mexico', 'Berlin', 'Programmers'])); // 7
-
 // Bonus - Iteration #4.1
 const mixedArr2 = [6, 12, 'miami', 1, true, 'barca', '200', 'lisboa', 8, 10]; // should return: 5.7
 
@@ -130,10 +92,6 @@ function avg(arr) {
   let total = sum(arr);
   return Math.round((total/arr.length) * 100) / 100;
 }
-
-// console.log(avg(mixedArr2)); // 5.7
-// console.log(avg([6, 12, 'miami', 1, 'barca', '200', 'lisboa', 8, false])); // 5.11
-// console.log(avg([6, 12, 'miami', 1, 'barca', '200', 'lisboa', 8, true])); // 5.22
 
 
 // Iteration #5: Unique arrays
@@ -151,16 +109,6 @@ const wordsUnique = [
   'bring'
 ];
 
-// function uniquifyArray(wordArr) {
-//   let filtered = [];
-//   for (let i=0; i < wordArr.length; i++) {
-//     if (wordArr.indexOf(wordArr[i], i+1) === -1) {
-//       filtered.unshift(wordArr[i]);
-//     }
-//   }
-//   return filtered;
-// }
-
 function uniquifyArray(wordArr) {
   if (wordArr.length === 0) {
     return null;
@@ -173,13 +121,6 @@ function uniquifyArray(wordArr) {
     } 
   return filtered;
 }
-
-// console.log(uniquifyArray(wordsUnique)); //['crab','poison','contagious','simple','bring','sharp','playground','communion']
-// console.log(uniquifyArray(['Ironhack', 'Ironhack', 'Ironhack'])); //Ironhack
-// console.log(uniquifyArray(['Cat', 'Dog', 'Cow'])); // ['Cat', 'Dog', 'Cow']
-// console.log(uniquifyArray(['iPhone', 'Samsung', 'Android', 'iOS', 'iPhone', 'Samsung', 'Nokia', 'Blackberry', 'Android'])); //['iPhone', 'Samsung', 'Android', 'iOS', 'Nokia', 'Blackberry'] 
-
-
 
 // Bonus - Iteration #5.1: Unique arrays using Set
 
@@ -196,10 +137,6 @@ function uniquifyArrayWithSet(wordArr) {
 }
 
 
-
-// console.log(uniquifyArrayWithSet(wordsUnique));  //['crab','poison','contagious','simple','bring','sharp','playground','communion']
-
-
 // Iteration #6: Find elements
 const wordsFind = ['machine', 'subset', 'trouble', 'starting', 'matter', 'eating', 'truth', 'disobedience'];
 
@@ -209,13 +146,6 @@ function doesWordExist(wordArr, word) {
   }
   return wordArr.includes(word);
 }
-
-// console.log(doesWordExist(wordsFind, 'nope')); // false
-// console.log(doesWordExist(wordsFind, 'matter')); // true
-// console.log(doesWordExist([])); // null ??
-// console.log(doesWordExist(['machine'], 'machine')); // true
-// console.log(doesWordExist(['machine', 'poison', 'eat', 'apple', 'horse'], 'ratatouille')); // false
-// console.log(doesWordExist(['pizza', 'sandwich', 'snack', 'soda', 'book', 'computer'], 'book')); // true
 
 
 // Iteration #7: Count repetition
@@ -245,14 +175,6 @@ function howManyTimes(wordArr, word) {
   });
   return counter;
 }
-
-// console.log(howManyTimes(wordsCount, "matter")); // 4
-// console.log(howManyTimes(['basketball', 'football', 'tennis'], 'tennis')); // 1
-
-
-
-
-
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
